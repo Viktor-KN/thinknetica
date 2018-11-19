bc59f0e0618cba07208441f30254bbf7ca0f3357
Viktor Kachulin <vicart2008@gmail.com>
2018-11-19 18:28:51 +1000

    Added lesson_7

---
+++ lesson_7/route.rb
@@ -0,0 +1,41 @@
+class Route
+  include Instances
+  include Valid
+
+  attr_reader :stations
+
+  def initialize(first_station, last_station)
+    @stations = [first_station, last_station]
+    validate!
+    register_instance
+  end
+
+  def add_station(station)
+    raise "Station #{station} already in route." if stations.include?(station)
+
+    stations.insert(-2, station)
+  end
+
+  def remove_station(station)
+    raise "Can't remove first or last station." if station == stations.first ||
+                                                   station == stations.last
+
+    stations.delete(station)
+  end
+
+  def to_s(highlight_station = nil)
+    title = ''
+    stations.each do |station|
+      ast = station == highlight_station ? '*' : ''
+      title += "#{ast}#{station.name}#{ast}" \
+               "#{station == stations.last ? '' : ' - '}"
+    end
+    title
+  end
+
+  protected
+
+  def validate!
+    raise 'First and last stations must vary.' if @stations.first == @stations.last
+  end
+end
---
+++ lesson_7/station.rb
@@ -0,0 +1,51 @@
+class Station
+  include Instances
+  include Valid
+
+  def self.find(name)
+    instances.find { |instance| instance.name.casecmp(name).zero? }
+  end
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @trains = []
+    validate!
+    register_instance
+  end
+
+  def add_train(train)
+    trains << train
+  end
+
+  def remove_train(train)
+    trains.delete(train)
+  end
+
+  def trains(type = nil)
+    if type
+      @trains.select { |train| train.class == type }
+    else
+      @trains
+    end
+  end
+
+  def each_train
+    raise 'No block given.' unless block_given?
+
+    trains.each { |train| yield(train) }
+  end
+
+  def to_s
+    name
+  end
+
+  protected
+
+  def validate!
+    raise 'Name must be at least 2 chars long.' if name.length < 2
+
+    raise 'Station with that name already exist.' if self.class.find(name)
+  end
+end
